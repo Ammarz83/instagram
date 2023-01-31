b95296a31b5302b098626122c293af064991b4d8
Ammarz83~ <sapitovammar@gmail.com>
2023-01-31 17:15:42 +0600

    commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function input() {
+  return (
+    <label>
+        <input type="text" />
+    </label>
+  )
+}
+
+export default input
---
+++ src/pages/loginPage/loginPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Input from "../../components/input/Input"
+
+function loginPage() {
+  return  (
+  <div>
+    <Input />
+  </div>
+  )
+}
+
+export default loginPage
